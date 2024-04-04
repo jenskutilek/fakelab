9a4955df427dc34432a517b2dfee7b1c133e2153
Jens Kutilek <webmail@kutilek.de>
2024-04-04 17:24:41 +0200

    ListParent fixes

--- lib/FL/helpers/ListParent.py
+++ lib/FL/helpers/ListParent.py
@@ -3,10 +3,10 @@ from collections import UserList
 
 class ListParent(UserList):
     """
-    Like a list, but sets child element's parent attribute when appending.
+    Like a list, but sets child element's _parent attribute when appending.
     """
 
-    def __init__(self, value=[], parent=None):
+    def __init__(self, value=None, parent=None):
         """
         >>> pl = ListParent([1, 2, 3], 4)
         >>> print(pl)
@@ -16,37 +16,40 @@ class ListParent(UserList):
         """
         super(UserList, self).__init__()
         self._parent = parent
+        if value is None:
+            value = []
         self.data = value
 
     def __add__(self, value):
-        value.parent = self._parent
+        value._parent = self._parent
         return self.data.__add__(value)
 
     def __iadd__(self, value):
-        value.parent = self._parent
+        value._parent = self._parent
         self.data.__iadd__(value)
 
     def __setitem__(self, index, value):
-        value.parent = self._parent
+        value._parent = self._parent
         self.data[index] = value
 
     def __setslice__(self, i, j, value):
         raise NotImplementedError
 
     def append(self, obj):
-        obj.parent = self._parent
+        obj._parent = self._parent
         self.data.append(obj)
 
     def extend(self, iterable):
         for obj in iterable:
+            obj._parent = self._parent
             self.data.append(obj)
 
     def insert(self, index, obj):
         """
         >>> pl = ListParent([1, 3])
         """
-        obj.parent = self._parent
-        self.data.insert(obj)
+        obj._parent = self._parent
+        self.data.insert(index, obj)
 
 
 if __name__ == "__main__":
