bc3f0fd9ddb310421777a9630710bb583b62cc69
Jens Kutilek <webmail@kutilek.de>
2025-04-07 14:02:35 +0200

    Fix Matrix so it matches FL

--- src/FL/objects/Matrix.py
+++ src/FL/objects/Matrix.py
@@ -2,23 +2,16 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from FL.helpers.transform import Transform
-
 if TYPE_CHECKING:
     from FL.objects.Point import Point
 
 
 class Matrix:
     """
-    ===============================================
     Matrix - class to represent planar transformations
-    ===============================================
-    Matrix is used to perform following transformations:
-    x1 = x * a + y * b + e
-    y1 = x * c + y * d + f
     """
 
-    __slots__ = ["_transform"]
+    __slots__ = ["_a", "_b", "_c", "_d", "_e", "_f"]
 
     def __init__(
         self,
@@ -46,7 +39,12 @@ class Matrix:
             e (float | None, optional): dx. Defaults to None.
             f (float | None, optional): dy. Defaults to None.
         """
-        self._transform = Transform(1, 0, 0, 1, 0, 0)
+        self._a: float = 1
+        self._b: float = 0
+        self._c: float = 0
+        self._d: float = 1
+        self._e: float = 0
+        self._f: float = 0
         if a is not None:
             self.Assign(a, b, c, d, e, f)
 
@@ -59,99 +57,69 @@ class Matrix:
     # FakeLab additions
 
     def fake_transform_point(self, p: Point) -> None:
-        p.x, p.y = self._transform.transformPoint((p.x, p.y))
+        """
+        Matrix is used to perform following transformations:
+
+            x1 = x * a + y * b + e
+            y1 = x * c + y * d + f
+
+        Args:
+            p (Point): The point to be transformed
+        """
+        x = p.x
+        y = p.y
+        p.x = x * self.a + y * self.b + self.e
+        p.y = x * self.c + y * self.d + self.f
 
     # Attributes
 
     @property
     def a(self) -> float:
-        return self._transform.xx
+        return self._a
 
     @a.setter
     def a(self, value: float) -> None:
-        self._transform = Transform(
-            float(value),
-            self._transform.xy,
-            self._transform.yx,
-            self._transform.yy,
-            self._transform.dx,
-            self._transform.dy,
-        )
+        self._a = float(value)
 
     @property
     def b(self) -> float:
-        return self._transform.xy
+        return self._b
 
     @b.setter
     def b(self, value: float) -> None:
-        self._transform = Transform(
-            self._transform.xx,
-            float(value),
-            self._transform.yx,
-            self._transform.yy,
-            self._transform.dx,
-            self._transform.dy,
-        )
+        self._b = float(value)
 
     @property
     def c(self) -> float:
-        return self._transform.yx
+        return self._c
 
     @c.setter
     def c(self, value: float) -> None:
-        self._transform = Transform(
-            self._transform.xx,
-            self._transform.xy,
-            float(value),
-            self._transform.yy,
-            self._transform.dx,
-            self._transform.dy,
-        )
+        self._c = float(value)
 
     @property
     def d(self) -> float:
-        return self._transform.yy
+        return self._d
 
     @d.setter
     def d(self, value: float) -> None:
-        self._transform = Transform(
-            self._transform.xx,
-            self._transform.xy,
-            self._transform.yx,
-            float(value),
-            self._transform.dx,
-            self._transform.dy,
-        )
+        self._d = float(value)
 
     @property
     def e(self) -> float:
-        return self._transform.dx
+        return self._e
 
     @e.setter
     def e(self, value: float) -> None:
-        self._transform = Transform(
-            self._transform.xx,
-            self._transform.xy,
-            self._transform.yx,
-            self._transform.yy,
-            float(value),
-            self._transform.dy,
-        )
+        self._e = float(value)
 
     @property
     def f(self) -> float:
-        return self._transform.dy
+        return self._f
 
     @f.setter
     def f(self, value: float) -> None:
-        self._transform = Transform(
-            self._transform.xx,
-            self._transform.xy,
-            self._transform.yx,
-            self._transform.yy,
-            self._transform.dx,
-            float(value),
-        )
+        self._f = float(value)
 
     # Operations
 
@@ -216,68 +184,73 @@ class Matrix:
         assigns new values to a Matrix, uses the same syntax as in constructors
         """
         if isinstance(a, Matrix):
-            self._transform = Transform(
-                a._transform.xx,
-                a._transform.xy,
-                a._transform.yx,
-                a._transform.yy,
-                a._transform.dx,
-                a._transform.dy,
-            )
+            self._a = a.a
+            self._b = a.b
+            self._c = a.c
+            self._d = a.d
+            self._e = a.e
+            self._f = a.f
         elif isinstance(a, list):
             assert len(a) == 6
-            self._transform = Transform(*a)
+            self._a, self._b, self._c, self._d, self._e, self._f = a
         else:
             for v in (a, b, c, d, e, f):
                 if not (isinstance(v, float) or isinstance(v, int)):
                     raise RuntimeError
-            self._transform = Transform(a, b, c, d, e, f)
+            self._a = a
+            self._b = b
+            self._c = c
+            self._d = d
+            self._e = e
+            self._f = f
 
     def Add(self, m: Matrix) -> None:
         """
         Adds values of the Matrix m to current matrix
         """
-        self._transform = Transform(
-            self.a + m.a,
-            self.b + m.b,
-            self.c + m.c,
-            self.d + m.d,
-            self.e + m.e,
-            self.f + m.f,
-        )
+        self.a += m.a
+        self.b += m.b
+        self.c += m.c
+        self.d += m.d
+        self.e += m.e
+        self.f += m.f
 
     def Sub(self, m: Matrix) -> None:
         """
         Subtracts values of the Matrix m from current matrix
         """
-        self._transform = Transform(
-            self.a - m.a,
-            self.b - m.b,
-            self.c - m.c,
-            self.d - m.d,
-            self.e - m.e,
-            self.f - m.f,
-        )
+        self.a -= m.a
+        self.b -= m.b
+        self.c -= m.c
+        self.d -= m.d
+        self.e -= m.e
+        self.f -= m.f
 
     def Mul(self, s: float) -> None:
         """
         Mutiplies Matrix's parameters to s value
         """
-        self._transform = Transform(
-            self.a * s,
-            self.b * s,
-            self.c * s,
-            self.d * s,
-            self.e * s,
-            self.f * s,
-        )
+        self.a *= s
+        self.b *= s
+        self.c *= s
+        self.d *= s
+        self.e *= s
+        self.f *= s
 
     def Transform(self, m: Matrix) -> None:
         """
-        applies Matrix m transformation to the current Matrix
+        Applies Matrix m transformation to the current Matrix
         """
-        dx = self.e
-        dy = self.f
-        self._transform = self._transform.transform(m._transform)
-        xx, xy, yx, yy, dx2, dy2 = self._transform
-        self._transform = Transform(xx, xy, yx, yy, dx2, dy2)
+        A = [[self.a, self.c], [self.b, self.d], [self.e, self.f]]
+        B = [[m.a, m.c], [m.b, m.d]]
+        result = [
+            [sum(a * b for a, b in zip(A_row, B_col)) for B_col in zip(*B)]
+            for A_row in A
+        ]
+        (a, c), (b, d), (e, f) = result
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.e = e + m.e
+        self.f = f + m.f
--- tests/objects/matrix_test.py
+++ tests/objects/matrix_test.py
@@ -3,48 +3,90 @@ import unittest
 import pytest
 
 from FL.objects.Matrix import Matrix
+from FL.objects.Point import Point
 
 
 class MatrixTests(unittest.TestCase):
     def test_instantiation(self):
         m = Matrix()
-        assert m.a == 1.0
-        assert m.b == 0.0
-        assert m.c == 0.0
-        assert m.d == 1.0
-        assert m.e == 0.0
-        assert m.f == 0.0
+        assert m.a == 1
+        assert m.b == 0
+        assert m.c == 0
+        assert m.d == 1
+        assert m.e == 0
+        assert m.f == 0
 
-    def test_Mul_float(self):
-        m1 = Matrix(2, 2, 2, 2, 2, 2)
-        m1.Mul(3)
-        assert m1.a == 6
-        assert m1.b == 6
-        assert m1.c == 6
-        assert m1.d == 6
-        assert m1.e == 6
-        assert m1.f == 6
+    # Addition
 
-    def test_imul_float(self):
-        m1 = Matrix(2, 2, 2, 2, 2, 2)
-        m1 *= 3
-        assert m1.a == 6
-        assert m1.b == 6
-        assert m1.c == 6
-        assert m1.d == 6
-        assert m1.e == 6
-        assert m1.f == 6
+    def test_add(self):
+        m1 = Matrix(2, 4, 8, 16, 32, 64)
+        m2 = Matrix(3, 5, 7, 9, 11, 13)
+        mr = m1 + m2
+        assert mr.a == 5
+        assert mr.b == 9
+        assert mr.c == 15
+        assert mr.d == 25
+        assert mr.e == 43
+        assert mr.f == 77
 
-    def test_imul_matrix(self):
-        m1 = Matrix(2, 2, 2, 2, 2, 2)
-        m2 = Matrix(3, 3, 3, 3, 3, 3)
-        m1 *= m2
-        assert m1.a == 12
-        assert m1.b == 12
-        assert m1.c == 12
-        assert m1.d == 12
-        assert m1.e == 15
-        assert m1.f == 15
+    def test_iadd(self):
+        m = Matrix(2, 4, 8, 16, 32, 64)
+        m2 = Matrix(3, 5, 7, 9, 11, 13)
+        m += m2
+        assert m.a == 5
+        assert m.b == 9
+        assert m.c == 15
+        assert m.d == 25
+        assert m.e == 43
+        assert m.f == 77
+
+    def test_Add(self):
+        m = Matrix(2, 4, 8, 16, 32, 64)
+        m2 = Matrix(3, 5, 7, 9, 11, 13)
+        m.Add(m2)
+        assert m.a == 5
+        assert m.b == 9
+        assert m.c == 15
+        assert m.d == 25
+        assert m.e == 43
+        assert m.f == 77
+
+    # Subtraction
+
+    def test_sub(self):
+        m1 = Matrix(2, 4, 8, 16, 32, 64)
+        m2 = Matrix(3, 5, 7, 9, 11, 13)
+        mr = m1 - m2
+        assert mr.a == -1
+        assert mr.b == -1
+        assert mr.c == 1
+        assert mr.d == 7
+        assert mr.e == 21
+        assert mr.f == 51
+
+    def test_isub(self):
+        m = Matrix(2, 4, 8, 16, 32, 64)
+        m2 = Matrix(3, 5, 7, 9, 11, 13)
+        m -= m2
+        assert m.a == -1
+        assert m.b == -1
+        assert m.c == 1
+        assert m.d == 7
+        assert m.e == 21
+        assert m.f == 51
+
+    def test_Sub(self):
+        m = Matrix(2, 4, 8, 16, 32, 64)
+        m2 = Matrix(3, 5, 7, 9, 11, 13)
+        m.Sub(m2)
+        assert m.a == -1
+        assert m.b == -1
+        assert m.c == 1
+        assert m.d == 7
+        assert m.e == 21
+        assert m.f == 51
+
+    # Multiplication
 
     def test_mul_float(self):
         m1 = Matrix(2, 2, 2, 2, 2, 2)
@@ -67,6 +109,39 @@ class MatrixTests(unittest.TestCase):
         assert m1.e == 15
         assert m1.f == 15
 
+    def test_imul_float(self):
+        m1 = Matrix(2, 2, 2, 2, 2, 2)
+        m1 *= 3
+        assert m1.a == 6
+        assert m1.b == 6
+        assert m1.c == 6
+        assert m1.d == 6
+        assert m1.e == 6
+        assert m1.f == 6
+
+    def test_imul_matrix(self):
+        m1 = Matrix(2, 2, 2, 2, 2, 2)
+        m2 = Matrix(3, 3, 3, 3, 3, 3)
+        m1 *= m2
+        assert m1.a == 12
+        assert m1.b == 12
+        assert m1.c == 12
+        assert m1.d == 12
+        assert m1.e == 15
+        assert m1.f == 15
+
+    def test_Mul_float(self):
+        m1 = Matrix(2, 2, 2, 2, 2, 2)
+        m1.Mul(3)
+        assert m1.a == 6
+        assert m1.b == 6
+        assert m1.c == 6
+        assert m1.d == 6
+        assert m1.e == 6
+        assert m1.f == 6
+
+    # Transform
+
     def test_Transform(self):
         m1 = Matrix(2, 2, 2, 2, 2, 2)
         m2 = Matrix(3, 3, 3, 3, 3, 3)
@@ -80,11 +155,25 @@ class MatrixTests(unittest.TestCase):
 
     def test_Transform_2(self):
         m1 = Matrix(2, 4, 8, 16, 32, 64)
-        m2 = Matrix(3, 3, 3, 3, 3, 3)
+        m2 = Matrix(3, 5, 7, 9, 11, 13)
         m1 = m1 * m2
-        assert m1.a == 30
-        assert m1.b == 60
-        assert m1.c == 30
-        assert m1.d == 60
-        assert m1.e == 291
-        assert m1.f == 291
+        assert m1.a == 46
+        assert m1.b == 92
+        assert m1.c == 86
+        assert m1.d == 172
+        assert m1.e == 427
+        assert m1.f == 813
+
+    # FakeLab addition
+
+    def test_Transform_Point(self):
+        m1 = Matrix(2, 4, 8, 16, 32, 64)
+        p = Point(44, 55)
+        p.Transform(m1)
+        assert (p.x, p.y) == (340, 1296)
+
+    def test_Transform_Point_float(self):
+        m1 = Matrix(2, 4, 8, 16, 32, 64)
+        p = Point(44.2, 55.5)
+        p.Transform(m1)
+        assert (p.x, p.y) == (342.4, 1305.6)
