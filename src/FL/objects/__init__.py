2d731ad184ea7c2943bc7b0babce985402c174a0
Jens Kutilek <webmail@kutilek.de>
2025-04-07 14:36:29 +0200

    Add placeholders for remaining objects

--- src/FL/objects/__init__.py
+++ src/FL/objects/__init__.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
+from FL.objects.Anchor import Anchor
+from FL.objects.AuditRecord import AuditRecord
+from FL.objects.Canvas import Canvas
 from FL.objects.Component import Component
+from FL.objects.Dialog import Dialog
 from FL.objects.Encoding import Encoding
 from FL.objects.EncodingRecord import EncodingRecord
 from FL.objects.Feature import Feature
@@ -9,18 +13,23 @@ from FL.objects.FontLab import FakeLab
 from FL.objects.Glyph import Glyph
 from FL.objects.Guide import Guide
 from FL.objects.Hint import Hint
+from FL.objects.Image import Image
 from FL.objects.KerningPair import KerningPair
 from FL.objects.Link import Link
 from FL.objects.Matrix import Matrix
+from FL.objects.NameRecord import NameRecord
 from FL.objects.Node import Node
 from FL.objects.Options import Options
 from FL.objects.Point import Point
 from FL.objects.Rect import Rect
+from FL.objects.Replace import Replace
+from FL.objects.TrueTypeTable import TrueTypeTable
 from FL.objects.TTH import TTH
 from FL.objects.TTHCommand import TTHCommand
 from FL.objects.TTHPoint import TTHPoint
 from FL.objects.TTInfo import TTInfo
 from FL.objects.TTPoint import TTPoint
+from FL.objects.TTStem import TTStem
 
 __all__ = [
     "Anchor",
@@ -54,44 +63,3 @@ __all__ = [
     "TTPoint",
     "TTStem",
 ]
-
-
-# Classes below are only placeholders that do nothing.
-# If you need any of them in your testing code, move them to a separate file
-# and implement them as far as you need.
-
-
-class Anchor:
-    pass
-
-
-class AuditRecord:
-    pass
-
-
-class Canvas:
-    pass
-
-
-class Dialog:
-    pass
-
-
-class Image:
-    pass
-
-
-class NameRecord:
-    pass
-
-
-class Replace:
-    pass
-
-
-class TrueTypeTable:
-    pass
-
-
-class TTStem:
-    pass
