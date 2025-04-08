11764296be91643d686a4812485dd573e3ebf91b
Jens Kutilek <webmail@kutilek.de>
2025-04-08 10:22:03 +0200

    Add TTPoint

--- src/FL/objects/TTPoint.py
+++ src/FL/objects/TTPoint.py
@@ -6,23 +6,75 @@ class TTPoint:
     ===============================================
     TTPoint - class to represent TrueType points
     ===============================================
+
     All coordinates in TTPoint class are set in 10.6 fixed-point format
     """
 
-    __slots__ = ["x", "y", "flag"]
+    __slots__ = ["_x", "_y", "_flag"]
 
     # Constructor
 
     def __init__(self) -> None:
+        """
+        TTPoint() - generic constructor, creates a TTPoint with zero coordinates
+        """
         self.x = 32767
         self.y = 32767
         self.flag = 255
-        # Can I has a parent?
-        # self._parent = None
 
     def __repr__(self) -> str:
-        return '<TTPoint flags="%i" x="%g" y="%g", Orphan>' % (
-            self.flag,
-            self.x,
-            self.y,
-        )
+        return f"<TTPoint: flags: {self.flag}, x: {self.x} y: {self.y}, Orphan>"
+
+    # Attributes
+
+    @property
+    def x(self) -> int:
+        """
+        x coordinate
+
+        Returns:
+            int: _description_
+        """
+        return self._x
+
+    @x.setter
+    def x(self, value: int) -> None:
+        if not isinstance(value, int):
+            raise TypeError
+        self._x = value
+
+    @property
+    def y(self) -> int:
+        """
+        y coordinate
+
+        Returns:
+            int: _description_
+        """
+        return self._y
+
+    @y.setter
+    def y(self, value: int) -> None:
+        if not isinstance(value, int):
+            raise TypeError
+        self._y = value
+
+    @property
+    def flag(self) -> int:
+        """
+        point's flag (on-curve or off-curve)
+
+        Returns:
+            int: _description_
+        """
+        return self._flag
+
+    @flag.setter
+    def flag(self, value: int) -> None:
+        if not isinstance(value, int):
+            raise TypeError
+        self._flag = value
+
+    # Operations: none
+
+    # Methods: none
