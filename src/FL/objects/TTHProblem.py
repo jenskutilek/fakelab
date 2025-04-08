6d044a8e4d77ff3f770e67c3dee12566b93a4043
Jens Kutilek <webmail@kutilek.de>
2025-04-08 11:50:50 +0200

    Add undocumented TTHProblem

---
+++ src/FL/objects/TTHProblem.py
@@ -0,0 +1,27 @@
+"""
+===============================================
+  TTHProblem - class to represent TrueType problems
+===============================================
+
+CONSTRUCTOR:
+  Class TTHProblem is auxiliary and cannot be created explicitly
+
+ATTRIBUTES (all read-only):
+  id (integer) - problem's id
+  command (integer) - problem command's index
+  direction(integer) - direction of problem's command
+
+OPERATIONS:
+
+METHODS
+
+Class TTHProblem is auxiliary and cannot be created explicitly
+
+TTHProblem
+
+<TTHProblem: Id: %d, Command: %d, Direction: %s> direction command
+"""
+
+
+class TTHProblem:
+    pass
