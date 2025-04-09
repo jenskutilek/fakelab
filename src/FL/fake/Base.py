61df01c153cc46bc6121d94d9d2dc6babea550c0
Jens Kutilek <webmail@kutilek.de>
2025-04-09 15:14:33 +0200

    Parent is not copied in copy constructors

--- src/FL/fake/Base.py
+++ src/FL/fake/Base.py
@@ -4,12 +4,17 @@ from typing import Any
 
 
 class Copyable:
-    __slots__: list[str] = []
+    __slots__: list[str] = ["_parent"]
 
     def _copy_constructor(self, other: Any) -> None:
         from copy import deepcopy
 
+        assert isinstance(other, type(self))
+
         tmp = deepcopy(other)
         for attr in self.__slots__:
-            # TODO: Is parent copied?
-            setattr(self, attr, getattr(tmp, attr))
+            if attr == "_parent":
+                # Parent is not copied
+                self._parent: Any | None = None
+            else:
+                setattr(self, attr, getattr(tmp, attr))
