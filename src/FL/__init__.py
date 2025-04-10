ecc17206d8dba073dcfd40f551ea66b0519b5f4b
Jens Kutilek <webmail@kutilek.de>
2025-04-10 11:28:36 +0200

    Move constants to separate file

--- src/FL/__init__.py
+++ src/FL/__init__.py
@@ -45,39 +45,6 @@ outside of FontLab Studio 5, and run tests.
 # fl is pre-instantiated
 fl = FakeLab()  # noqa: F405
 
-
-# Font formats
-
-# FontLab VFB font
-ftFONTLAB = 0
-
-# PC Type 1 font (binary/PFB)
-ftTYPE1 = 1
-
-# PC MultipleMaster font (PFB)
-ftTYPE1_MM = 20
-
-# PC Type 1 font (ASCII/PFA)
-ftTYPE1ASCII = 2
-
-# PC MultipleMaster font (ASCII/PFA)
-ftTYPE1ASCII_MM = 21
-
-# PC TrueType/TT OpenType font (TTF)
-ftTRUETYPE = 3
-
-# PS OpenType (CFF-based) font (OTF)
-ftOPENTYPE = 6
-
-# Mac Type 1 font (generates suitcase and LWFN file, optionally AFM)
-ftMACTYPE1 = 23
-
-# Mac TrueType font (generates suitcase)
-ftMACTRUETYPE = 30
-
-# Mac TrueType font (generates suitcase with resources in data fork)
-ftMACTRUETYPE_DFONT = 31
-
 # Everything that is needed for a convincing emulation:
 
 environment = {
