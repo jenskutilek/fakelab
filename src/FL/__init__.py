aa81c16ab399847648eb213c68f65a38307e2b4e
Jens Kutilek <webmail@kutilek.de>
2025-04-11 23:19:57 +0200

    Improve imports

--- src/FL/__init__.py
+++ src/FL/__init__.py
@@ -14,43 +14,41 @@ from FL.constants import (
     ftTYPE1ASCII_MM,
 )
 from FL.FLdict import *  # noqa: F403
-from FL.objects import (
-    TTH,
-    Anchor,
-    AuditRecord,
-    Canvas,
-    Component,
-    Dialog,
-    Encoding,
-    EncodingRecord,
-    FakeLab,
-    Feature,
-    Font,
-    Glyph,
-    Guide,
-    Hint,
-    Image,
-    KerningPair,
-    Link,
-    Matrix,
-    NameRecord,
-    Node,
-    Options,
-    Point,
-    Rect,
-    Replace,
-    TrueTypeTable,
-    TTGasp,
-    TTHCommand,
-    TTHPoint,
-    TTHProblem,
-    TTInfo,
-    TTPoint,
-    TTStem,
-    TTVdmx,
-    Uni,
-    WeightVector,
-)
+from FL.objects.Anchor import Anchor
+from FL.objects.AuditRecord import AuditRecord
+from FL.objects.Canvas import Canvas
+from FL.objects.Component import Component
+from FL.objects.Dialog import Dialog
+from FL.objects.Encoding import Encoding
+from FL.objects.EncodingRecord import EncodingRecord
+from FL.objects.Feature import Feature
+from FL.objects.Font import Font
+from FL.objects.FontLab import FakeLab
+from FL.objects.Glyph import Glyph
+from FL.objects.Guide import Guide
+from FL.objects.Hint import Hint
+from FL.objects.Image import Image
+from FL.objects.KerningPair import KerningPair
+from FL.objects.Link import Link
+from FL.objects.Matrix import Matrix
+from FL.objects.NameRecord import NameRecord
+from FL.objects.Node import Node
+from FL.objects.Options import Options
+from FL.objects.Point import Point
+from FL.objects.Rect import Rect
+from FL.objects.Replace import Replace
+from FL.objects.TrueTypeTable import TrueTypeTable
+from FL.objects.TTGasp import TTGasp
+from FL.objects.TTH import TTH
+from FL.objects.TTHCommand import TTHCommand
+from FL.objects.TTHPoint import TTHPoint
+from FL.objects.TTHProblem import TTHProblem
+from FL.objects.TTInfo import TTInfo
+from FL.objects.TTPoint import TTPoint
+from FL.objects.TTStem import TTStem
+from FL.objects.TTVdmx import TTVdmx
+from FL.objects.Uni import Uni
+from FL.objects.WeightVector import WeightVector
 
 """
 FakeLab. A FontLab Studio 5 replacement for testing Python code.
