171cf5184ba9123830bafb73203580d20d3d8376
Jens Kutilek <webmail@kutilek.de>
2025-06-12 16:57:27 +0200

    Serialize unicodes

--- src/FL/objects/Glyph.py
+++ src/FL/objects/Glyph.py
@@ -215,10 +215,10 @@ class Glyph(Copyable):
             # "mask"
             # "mask.metrics"
             # "mask.metrics_mm"
-            # "unicodes"
             # "Glyph Unicode Non-BMP"
             # "mark"
             "Glyph Origin": self._glyph_origin,
+            "unicodes": [u for u in self.unicodes if u <= 0xFFFF],
         }
         if self.kerning:
             s["Glyph"]["kerning"] = {
