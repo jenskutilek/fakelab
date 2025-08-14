aa88a7526cb8a28289fe6e18641cb87ca223252f
Jens Kutilek <webmail@kutilek.de>
2025-08-14 20:24:14 +0200

    Fix Python 3.10 compatibility

--- src/FL/helpers/math.py
+++ src/FL/helpers/math.py
@@ -5,11 +5,15 @@ def int32_to_unsigned(i: int) -> int:
     """
     Reinterpret a signed integer as unsigned
     """
-    return int.from_bytes(i.to_bytes(4, signed=True), signed=False)
+    return int.from_bytes(
+        i.to_bytes(4, signed=True, byteorder="big"), signed=False, byteorder="big"
+    )
 
 
 def uint32_to_signed(i: int) -> int:
     """
     Reinterpret an unsigned integer as signed
     """
-    return int.from_bytes(i.to_bytes(4, signed=False), signed=True)
+    return int.from_bytes(
+        i.to_bytes(4, signed=False, byteorder="big"), signed=True, byteorder="big"
+    )
