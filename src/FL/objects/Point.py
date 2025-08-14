50ac7c25430100b827b8e1d07d9fefc8d251f7ed
Jens Kutilek <webmail@kutilek.de>
2025-08-14 15:26:01 +0200

    Docs

--- src/FL/objects/Point.py
+++ src/FL/objects/Point.py
@@ -4,6 +4,7 @@ from typing import Any
 
 from FL.fake.Base import Copyable
 from FL.objects.Matrix import Matrix
+from FL.objects.Rect import Rect
 
 __doc__ = "Class to represent a point"
 
@@ -19,10 +20,37 @@ class Point(Copyable):
         """
         Point - base class to represent point
 
+        Point()
+            generic constructor, creates a Point with zero coordinates
+        Point(Point)
+            copy constructor
+        Point(x, y)
+            creates a Point and assigns coordinates. x and y may be integer or float
+
         Args:
             p_or_x (Point | float | None, optional): The `Point` to copy coordinates
                 from, or the x coordinate. Defaults to None.
             y (float | None, optional): The y coordinate. Defaults to None.
+        """
+        # From the binary:
+        """
+        Possible errors:
+
+        Incorrect # of args to:
+        Point()
+        Point(number x, number y)
+        Point(Point p)
+
+        Number is expected in arg 2:
+        Point(number x, number y)
+
+        Number is expected in arg 1:
+        Point(number x, number y)
+
+        Point type is expected in arg 1:
+        Point(Point p)
+
+        class Point has no attribute %s or it is read-only
         """
         self._parent = None
         self.x = 0
@@ -83,12 +111,15 @@ class Point(Copyable):
     # Operations
 
     def __eq__(self, other: object) -> bool:
+        """
+        compares two points, both coordinates must be equal
+        """
+        # Point expected to compare
         if not isinstance(other, Point):
             return False
         return self.x == other.x and self.y == other.y
 
-    # coerce?
-    def __coerce__(self, other):
+    def __coerce__(self, other: Point | float | Matrix | Rect) -> Point:
         """
         can be operated on Point, float value, Matrix and Rect
         """
@@ -98,12 +129,16 @@ class Point(Copyable):
         """
         Point must be second operand, both coordinates are added
         """
+        if not isinstance(other, Point):
+            raise RuntimeError("Point is expected as right operand of Point.operator+")
         return Point(self.x + other.x, self.y + other.y)
 
     def __sub__(self, other: Point) -> Point:
         """
         Point must be second operand, both coordinates are subtracted
         """
+        if not isinstance(other, Point):
+            raise RuntimeError("Point is expected as right operand of Point.operator-")
         return Point(self.x - other.x, self.y - other.y)
 
     def __mul__(self, other: Point | float | Matrix) -> Point | float:
@@ -111,6 +146,7 @@ class Point(Copyable):
         Second operand may be `Point`, `float` or `Matrix`. If second operand is
         `Point`, then result of scalar product is returned
         """
+        # Point is expected as left operand of Point.operator*
         if isinstance(other, Point):
             # Scalar product
             return self.x * other.x + self.y * other.y
@@ -121,13 +157,29 @@ class Point(Copyable):
             p.Transform(other)
             return p
 
-        return Point(self.x * other, self.y * other)
+        elif isinstance(other, (float, int)):
+            return Point(self.x * other, self.y * other)
+
+        raise RuntimeError(
+            "Point, Matrix or Number is expected as right operand of Point.operator*"
+        )
 
     # Methods
 
     def Assign(self, p_or_x: Point | float, y: float | None = None) -> None:
         """
         Assigns new values to a Point
+        """
+
+        """
+        Possible errors:
+
+        Incorrect # of args to:
+        Point.Assign(number x, number y)
+        Point.Assign(Point p)
+
+        Point is expected in arg 1:
+        Point.Assign(Point p)
         """
         if isinstance(p_or_x, Point):
             # copy
@@ -150,6 +202,17 @@ class Point(Copyable):
         """
         Shifts Point on a position defined by p or x and y values
         """
+
+        """
+        Possible errors:
+        
+        Incorrect # of args to:
+        Point.Shift(number x, number y)
+        Point.Shift(Point p)
+        
+        Point is expected in arg 1:
+        Point.Shift(Point p)
+        """
         if isinstance(p_or_x, Point):
             p = p_or_x
         else:
@@ -161,11 +224,31 @@ class Point(Copyable):
         """
         Same as Shift(Point p)
         """
+
+        """
+        Possible errors:
+        
+        Incorrect # of args to:
+        Point.Add(Point p)
+        
+        Point is expected in arg 1:
+        Point.Add(Point p)
+        """
         self.Shift(p)
 
     def Sub(self, p: Point) -> None:
         """
         Subtracts p coordinates from the current Point
+        """
+
+        """
+        Possible errors:
+        
+        Incorrect # of args to:
+        Point.Sub(Point p).
+        
+        Point is expected in arg 1:
+        Point.Sub(Point p)
         """
         self.x -= p.x
         self.y -= p.y
@@ -174,11 +257,31 @@ class Point(Copyable):
         """
         Multiplies Point's position to s value
         """
+
+        """
+        Possible errors:
+
+        Incorrect # of args to:
+        Point.Mul(number n).
+        
+        Number is expected in arg 1:
+        Point.Mul(number n)
+        """
         self.x *= s
         self.y *= s
 
     def Transform(self, m: Matrix) -> None:
         """
         Applies Matrix transformation to the Point
+        """
+
+        """
+        Possible errors:
+
+        Incorrect # of args to:
+        Point.Transform(Matrix m).
+        
+        Matrix is expected in arg 1:
+        Point.Transform(Matrix m)
         """
         m.fake_transform_point(self)
