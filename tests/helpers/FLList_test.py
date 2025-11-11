05829cae79e4ffa7ff526848b1a9e57d7a02eb1c
Jens Kutilek <webmail@kutilek.de>
2025-11-11 19:01:52 +0100

    Annotate FLList tests

--- tests/helpers/FLList_test.py
+++ tests/helpers/FLList_test.py
@@ -6,43 +6,43 @@ from FL.helpers.FLList import adjust_list
 
 
 class FLListTests(unittest.TestCase):
-    def test_adjust_list_equal(self):
+    def test_adjust_list_equal(self) -> None:
         seq = [0, 0, 0, 0]
         expected = [0, 0, 0, 0]
         adjust_list(seq, 4)
         assert seq == expected
 
-    def test_adjust_list_empty(self):
-        seq = []
+    def test_adjust_list_empty(self) -> None:
+        seq: list[None] = []
         expected = [None, None, None]
         adjust_list(seq, 3)
         assert seq == expected
 
-    def test_adjust_list_empty_value(self):
-        seq = []
+    def test_adjust_list_empty_value(self) -> None:
+        seq: list[int] = []
         expected = [0, 0, 0]
         adjust_list(seq, 3, 0)
         assert seq == expected
 
-    def test_adjust_list_no_value(self):
+    def test_adjust_list_no_value(self) -> None:
         seq = [0]
         expected = [0, 0, 0]
         adjust_list(seq, 3)
         assert seq == expected
 
-    def test_adjust_list_value(self):
+    def test_adjust_list_value(self) -> None:
         seq = [1]
         expected = [1, 0, 0]
         adjust_list(seq, 3, 0)
         assert seq == expected
 
-    def test_adjust_list_shorter_no_value(self):
+    def test_adjust_list_shorter_no_value(self) -> None:
         seq = [0, 0, 0]
         expected = [0]
         adjust_list(seq, 1)
         assert seq == expected
 
-    def test_adjust_list_shorter_value(self):
+    def test_adjust_list_shorter_value(self) -> None:
         seq = [1, 0, 0]
         expected = [1]
         adjust_list(seq, 1, 0)  # value is ignored
